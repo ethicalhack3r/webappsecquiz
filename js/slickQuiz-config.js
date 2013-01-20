ffe863b96ab37aff6ceebea3cb287b600d814758
ethicalhack3r <ryandewhurst@gmail.com>
2013-01-20 12:48:15 +0100

    Added HSTS question.

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -6,7 +6,7 @@ var quizJSON = {
     "info": {
         "name":    "Web App Sec Quiz",
         "main":    "<p>Are you a security professional or developer? Test your Web App Sec knowledge!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
+        "results": "<h5>Learn More</h5> <p><a href='https://www.owasp.org/index.php/Main_Page'>OWASP</a></p>",
         "level1":  "Rockstar! You are ready to pwn the world!",
         "level2":  "Nice score! Some gaps in your knowledge, all you need is time.",
         "level3":  "Not bad, do some more practice. Download OWASP's Broken Web Apps VM",
@@ -48,13 +48,13 @@ var quizJSON = {
             "incorrect": "<p><span>No.</span> Never store passwords! See: <a href='https://www.owasp.org/index.php/Password_Storage_Cheat_Sheet' target='_blank'>https://www.owasp.org/index.php/Password_Storage_Cheat_Sheet</a></p>" // no comma here
         },
         { // Question 4
-            "q": "Is Earth bigger than a basketball?",
+            "q": "The HTTP Strict Transport Security (HSTS) header ensures (as well as other things) the domain is only accessed over HTTPS. (if the browser supports it)",
             "a": [
-                {"option": "Yes",    "correct": true},
-                {"option": "No",     "correct": false} // no comma here
+                {"option": "True",    "correct": true},
+                {"option": "False",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+            "correct": "<p><span>Good Job!</span> You know your headers!</p>",
+            "incorrect": "<p><span>ERRRR!</span> See: <a href='https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security' target='_blank'>https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security</a></p>" // no comma here
         },
         { // Question 5
             "q": "Where are you right now? Select ALL that apply.",
